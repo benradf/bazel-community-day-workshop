2e372d44a65ee78967892a15ece4d295df397e22
Ben Radford <benradf@users.noreply.github.com>
2023-10-23 08:51:57 +0100

    Include username and date in execlog filenames.

--- compare_exec_logs.sh
+++ compare_exec_logs.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+set -eux
+
 #   This script executes bazel build command twice to produce
 #   execution logs, then downloads the tool to compare the logs
 #   and calls it.
 #   Visit https://github.com/JSGette/remote-cache-debugger to
 #   know more about the tool.
 
+exec-log-filename() {
+    mkdir -p /tmp/execlogs
+    printf "/tmp/execlogs/exec-%s-%s.log" "$USER" $(date +%Y%m%d-%H%M%S)
+}
+
+extra_build_params=""  # --config=bcd-cluster  # --disk_cache=/tmp/disk_cache
+
 echo "Creating temporary disk cache directory..."
 mkdir -p /tmp/disk_cache
-bazel clean --expunge
+bazel clean #--expunge
 
+execlog1=$(exec-log-filename)
 echo "Performing first build to generate execution logs..."
-bazel build //... --disk_cache=/tmp/disk_cache --execution_log_binary_file=/tmp/exec1.log
+bazel build //... $extra_build_params --execution_log_binary_file=$execlog1
 
 echo "Cleaning outputs of the previous build..."
-bazel clean --expunge
+bazel clean #--expunge
 
+execlog2=$(exec-log-filename)
 echo "Performing second build to generate execution logs..."
-bazel build //... --disk_cache=/tmp/disk_cache --execution_log_binary_file=/tmp/exec2.log
+bazel build //... $extra_build_params --execution_log_binary_file=$execlog2
 
 echo "Builds finished..."
 echo "============================================================="
@@ -26,7 +37,7 @@ echo "Downloading remote-cache-debugger..."
 curl -L https://github.com/JSGette/remote-cache-debugger/releases/download/0.0.1-SNAPSHOT/remote-cache-debugger-0.0.1-SNAPSHOT.jar > /tmp/rcd.jar
 
 echo "Comparing execution logs..."
-java -jar /tmp/rcd.jar -first /tmp/exec1.log -second /tmp/exec2.log -o output.txt
+java -jar /tmp/rcd.jar -first $execlog1 -second $execlog2 -o output.txt
 
 echo "Performing clean up..."
 rm -rf /tmp/disk_cache
